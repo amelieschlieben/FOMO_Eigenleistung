a45cf4d5a0a8bdb31c181a418ec232b281fded9c
Amelie <amelieschlieben@gmail.com>
2026-03-15 10:54:07 +0100

    update draft manuscript, update file structure

---
+++ simulation/EA03-analysis.R
@@ -0,0 +1,19 @@
+source("simulation/EA02-simulations.R")
+
+### Paket für ordinale logistische Regression installieren und laden
+install.packages("MASS")
+library("MASS")
+
+dfs$an_self_dis <- factor(
+  dfs$self_disclosure, 
+  levels = c("low", "medium", "high"),
+  ordered = TRUE
+)
+
+model <- polr(an_self_dis ~ anonymity * cues,
+              data = dfs,
+              Hess = TRUE)
+
+summary(model)
+exp(coef(model))
+exp(confint(model))
